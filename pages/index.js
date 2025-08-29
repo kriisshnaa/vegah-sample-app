cbc416c771b7c3c4c9819f0f9044ac09824dda15
kriisshnaa <132825175+kriisshnaa@users.noreply.github.com>
2025-08-29 19:09:45 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1>🚀 Vegah Sample Next.js + MongoDB App</h1>
-      <p>Deployed on Azure App Service with GitHub Actions ✅</p>
+      <p>CREATED BY KRISHNA JST FOR DEMO PURPOSE ✅</p>
     </div>
   );
 }
