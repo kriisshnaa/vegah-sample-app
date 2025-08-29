19c357f129bf352a231573120f10d5bbe95bdb9a
kriisshnaa <132825175+kriisshnaa@users.noreply.github.com>
2025-08-29 18:43:42 +0530

    Delete pages/pages directory

--- pages/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>🚀 Vegah Sample Next.js + MongoDB App</h1>
-      <p>Deployed on Azure App Service with GitHub Actions ✅</p>
-    </div>
-  );
-}
