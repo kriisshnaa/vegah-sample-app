fbf3270a9d7a62e6b545729f9c63fc50de1ef5a2
kriisshnaa <132825175+kriisshnaa@users.noreply.github.com>
2025-08-29 18:43:27 +0530

    Create index.js

---
+++ pages/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>🚀 Vegah Sample Next.js + MongoDB App</h1>
+      <p>Deployed on Azure App Service with GitHub Actions ✅</p>
+    </div>
+  );
+}
