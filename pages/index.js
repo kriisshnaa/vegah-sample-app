2434b66ab135b65ea43d43ce796d3ef1f604824b
kriisshnaa <132825175+kriisshnaa@users.noreply.github.com>
2025-08-29 19:46:55 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1>🚀 Vegah Sample Next.js + MongoDB App</h1>
-      <p>CREATED BY KRISHNA JUST FOR JOB PURPOSE ON 29/08/2025 FOR VEGAH. THIS WAS A DEMO VIDEO FOR ILLUSTRATION  ✅</p>
+      <p>CREATED BY KRISHNA JUST FOR JOB PURPOSE ON 29/08/2025 FOR VEGAH. THIS WAS A DEMO VIDEO FOR ILLUSTRATION MADE FOR DEVOPS JOB PROFILE ✅</p>
     </div>
   );
 }
